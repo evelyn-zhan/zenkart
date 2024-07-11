1cece800dad8e69263650fb73216d2be335e5a26
Evelyn <evelynzanpx@gmail.com>
2024-07-11 13:47:21 +0700

    Update: Cart page

--- src/components/AddNote.jsx
+++ src/components/AddNote.jsx
@@ -3,6 +3,12 @@ import React, { useState } from 'react';
 function AddNote({ id, note, trigger, setTrigger, onAddNote }) {
     const [ noteValue, setNoteValue ] = useState(note);
 
+    const noteLimit = 50;
+
+    if(noteLimit - noteValue.length <= 10) {
+        document.querySelector('.cart__product-add-note-limit').style.color = 'red';
+    }
+
     const onAddNoteHandler = (event) => {
         event.preventDefault();
         onAddNote(id, noteValue);
@@ -15,11 +21,14 @@ function AddNote({ id, note, trigger, setTrigger, onAddNote }) {
         <div className="cart__product-add-note">
             <span className="cart__product-add-note-text">Place a note for the seller...</span>
             <form className="cart__product-add-note-form" onSubmit={onAddNoteHandler}>
-                <input
-                    type="text" name="add-note" id={`add-note-${id}`}
-                    className="cart__product-add-note-input"
-                    value={noteValue} onChange={(event) => { setNoteValue(event.target.value); }}
-                />
+                <div className="cart__product-add-note-input-n-limit">
+                    <input
+                        type="text" name="add-note" id={`add-note-${id}`}
+                        className="cart__product-add-note-input"
+                        value={noteValue} onChange={(event) => { noteValue.length < noteLimit && setNoteValue(event.target.value); }}
+                    />
+                    <span className="cart__product-add-note-limit">{noteLimit - noteValue.length} characters left</span>
+                </div>
                 <button type="submit" className="cart__product-add-note-submit">Confirm</button>
                 <button className="cart__product-add-note-cancel" onClick={() => setTrigger(false)}>Cancel</button>
             </form>
